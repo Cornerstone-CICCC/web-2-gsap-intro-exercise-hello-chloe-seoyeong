4e16bd9eee50a1f0cbfa6a92ac6c3c2104b7f947
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-12-10 11:55:20 -0800

    Completed gsap exercise

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // PUT YOUR CODE HERE
+const timeline = gsap.timeline();
+
+timeline
+  .from(".red", {scale: 0, transformOrigin: "left top", duration: .5})
+  .from(".green", {scale: 0, transformOrigin: "right top", duration: .5})
+  .from(".blue", {scale: 0, transformOrigin: "right bottom", duration: .5})
+  .from(".yellow", {scale: 0, transformOrigin: "left bottom", duration: .5})
+  .to(".red", {borderRadius: "50%", left: "100vw", x: "-100%", backgroundColor: "green", duration: 1})
+  .to(".green", {borderRadius: "50%", top: "100vh", y: "-100%", right: 0, backgroundColor: "blue", duration: 1}, "<")
+  .to(".blue", {borderRadius: "50%", right: "100vw", x: "100%", backgroundColor: "yellow", duration: 1}, "<")
+  .to(".yellow", {borderRadius: "50%", bottom: "100vh", y: "100%", backgroundColor: "red", duration: 1}, "<")
+  .to(".red", {x: "+=100%", duration: 1})
+  .to(".green", {x: "+=100%", duration: 1}, "<")
+  .to(".blue", {x: "-=100%", duration: 1}, "<")
+  .to(".yellow", {x: "-=100%", duration: 1}, "<");
